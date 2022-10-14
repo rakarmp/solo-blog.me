94172ee9c46546665629f05cd00c2fa252e54757
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-10-14 14:52:47 +0700

    menambahkan assets

---
+++ resources/views/auth/components/link.blade.php
@@ -0,0 +1,16 @@
+<!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset('assets/Login/images/icons/favicon.ico')}}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/vendor/bootstrap/css/bootstrap.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/vendor/animate/animate.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/vendor/css-hamburgers/hamburgers.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/vendor/select2/select2.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('assets/Login/css/main.css')}}">
+<!--===============================================================================================-->
