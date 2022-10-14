899dc556ce30713c7f8066c275cda5dd85e7c4f9
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-10-14 14:51:16 +0700

    menambahkan controller

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('index');
+
